6bbc108ea53bf6cf0ce973fc0cbe477e72ed5559
vishalGaur <rr.vishalgaur@gmail.com>
2024-04-06 01:22:46 +0530

    adds spotless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,35 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.diffplug.spotless) apply false
+}
+
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
+        }
+
+        kotlin {
+            target ("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+            targetExclude("**/*.json")
+
+            trimTrailingWhitespace()
+            ktlint("0.49.0")
+        }
+
+        java {
+            target ("src/*/java/**/*.java")
+            targetExclude("$buildDir/**/*.java")
+            targetExclude("bin/**/*.java")
+            targetExclude("**/*.json")
+            trimTrailingWhitespace()
+            googleJavaFormat("1.11.0").aosp()
+        }
+    }
 }
