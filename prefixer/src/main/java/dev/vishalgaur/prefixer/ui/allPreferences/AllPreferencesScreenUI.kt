b786ef0d43eaf8a3ec3197c9a09630ac6e0bf6e1
vishalGaur <rr.vishalgaur@gmail.com>
2024-04-08 23:32:15 +0530

    adds AllPreferencesScreenUI.kt

---
+++ prefixer/src/main/java/dev/vishalgaur/prefixer/ui/allPreferences/AllPreferencesScreenUI.kt
@@ -0,0 +1,116 @@
+package dev.vishalgaur.prefixer.ui.allPreferences
+
+import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.vishalgaur.prefixer.ui.theme.PrefixerTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+internal fun AllPreferencesScreen(prefsName: String, prefsList: List<Pair<String, String>>) {
+    val listState = rememberLazyListState()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+            ) {
+                AllPreferencesTopAppBar(modifier = Modifier.fillMaxWidth())
+                Divider(modifier = Modifier.shadow(4.dp))
+            }
+        },
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+            state = listState,
+        ) {
+            stickyHeader {
+                PrefsNameStickyHeader(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 10.dp),
+                    prefsName = prefsName,
+                )
+            }
+            prefsList.forEachIndexed { index, pair ->
+                item(key = pair.first) {
+                    PreferencesPairItemView(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { }
+                            .padding(vertical = 8.dp, horizontal = 16.dp),
+                        key = pair.first,
+                        value = pair.second,
+                    )
+                    Divider()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PrefsNameStickyHeader(modifier: Modifier, prefsName: String) {
+    Row(
+        modifier = modifier.wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(text = "Prefs Name: ", modifier = Modifier.padding(end = 4.dp))
+        Text(
+            modifier = Modifier
+                .wrapContentHeight()
+                .padding(4.dp)
+                .weight(1f),
+            text = prefsName,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+    }
+}
+
+@Preview("Light Theme", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewAllPreferencesScreen() {
+    PrefixerTheme {
+        AllPreferencesScreen(
+            prefsName = "app_prefs",
+            prefsList = listOf(
+                "dsf" to "dsfdgh",
+                "dsfd" to "dsfwewreabdgh",
+                "dsfdfsgdd" to "dsfg dlif ksepodr jfg eprdo ;fjgewg dlif ksepodr jfg eprdo ;fjgewg dlif ksepodr jfg eprdo ;fjgewwsfg dlif ksepodr jfg eprdo ;fjgewreabdgh",
+            ),
+        )
+    }
+}
