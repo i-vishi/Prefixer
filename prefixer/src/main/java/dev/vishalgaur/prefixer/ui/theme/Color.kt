4cbfa90a0c7944bf7c5adb08303c2d99d0f2d16a
vishalGaur <rr.vishalgaur@gmail.com>
2024-04-08 23:31:33 +0530

    adds compose libraries, color, theme, styles to prefixer module

---
+++ prefixer/src/main/java/dev/vishalgaur/prefixer/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package dev.vishalgaur.prefixer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBackground = Color(0xFFF7F7F7)
+val DarkBackground = Color(0xFF2B2B2B)
+val KeyColor = Color(0xFFCC7832)
+val StringValueColor = Color(0xFF6A8759)
+val NumberColor = Color(0xFF6897BB)
+val BooleanColor = Color(0xFF2AA198)
