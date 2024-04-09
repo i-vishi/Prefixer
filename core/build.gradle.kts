5a19320a825a0523abf88c7664519e99f682ff2f
vishalGaur <rr.vishalgaur@gmail.com>
2024-04-10 00:24:10 +0530

    fixes core module to include compose options

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -22,6 +22,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
