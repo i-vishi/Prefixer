d35475f93fdc5a8437b33142d034227420063bec
vishalGaur <rr.vishalgaur@gmail.com>
2024-04-14 13:48:28 +0530

    adds compose ui dependency and loop drawable

--- prefixer/build.gradle.kts
+++ prefixer/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.runtime.livedata)
 
